f4fb0f680b5f18a60005245616f363b4f45632ea
DUMIYH-OFC-KOD <githubbot6@gmail.com>
2025-04-01 15:29:25 +0530

    ᴅᴀʀᴋ ᴅᴜᴍɪʏʜ ᴏꜰᴄ [ ᴋ.ᴏ.ᴅ ] ᵀᴹ ᴏꜰꜰɪᴄɪᴀʟ ʙᴏᴛ ⚡

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ᴅᴜᴍɪʏʜ-xᴍᴅ~7qgAgbKZ#fzUoknIVAZlcgzOUeBHoKk4-OFMPPTGNIepsN09y1Ik",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
